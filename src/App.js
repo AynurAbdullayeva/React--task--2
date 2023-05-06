98ea27843771a5f35e4775c5dc872fba6ec1e40a
aynur abdullayeva <7hkq5cl@code.edu.az>
2023-05-06 08:20:57 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from './Components/Header/Header';
+import About from './Components/About/About';
+import Footer from './Components/Footer/Footer';
+import Contact from './Components/Contact/Contact';
+import Products from './Components/Products/Products';
+
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <About/>
+    <Footer/>
+    <Contact/>
+    <Products/>
+    </>
+  );
+}
+
+export default App;
